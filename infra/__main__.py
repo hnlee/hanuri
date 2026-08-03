f8220ec32ef2e10dc1216e5b3d78e5631bc95925
Hana Lee <hanalee07@gmail.com>
2026-08-03 13:03:27 -0500

    Provision load balancer, SSL, HTTP-to-HTTPS

--- infra/__main__.py
+++ infra/__main__.py
@@ -2,7 +2,7 @@
 
 import pulumi
 import pathlib
-from pulumi_gcp import storage
+from pulumi_gcp import storage, compute
 
 env = pulumi.get_stack()
 
@@ -12,8 +12,7 @@ bucket = storage.Bucket(
     website={"main_page_suffix": "index.html"},
     uniform_bucket_level_access=True
 )
-
-cache_control = "no-cache" if env == "staging" else "max-age=3600"
+cache_control = "no-cache"
 
 build_dir = pathlib.Path(__file__).parent.parent / "dist"
 for path in build_dir.iterdir():
@@ -33,6 +32,29 @@ bucket_iam_binding = storage.BucketIAMBinding(
     members=["allUsers"],
 )
 
+
+if env == "prod":
+    backend_bucket = compute.BackendBucket(
+        "backend-bucket", bucket_name=bucket.name, enable_cdn=True
+    )
+    ip = compute.GlobalAddress("ip")
+    url_map = compute.URLMap("url-map", default_service=backend_bucket.id)
+    ssl = compute.ManagedSslCertificate("default", name="hanuri", managed={"domains": ["hanurikoreanschool.org", "www.hanurikoreanschool.org"]})
+    https_proxy = compute.TargetHttpsProxy("https-proxy", url_map=url_map.id, ssl_certificates=[ssl.id])
+    https_forwarding_rule = compute.GlobalForwardingRule(
+        "https-forwarding-rule",
+        ip_address=ip.address,
+        ip_protocol="TCP",
+        port_range="443",
+        target=https_proxy.id,
+        load_balancing_scheme="EXTERNAL_MANAGED"
+    )
+    redirect_url_map = compute.URLMap("redirect-map", default_url_redirect={"https_redirect": True, "strip_query": False})
+    http_proxy = compute.TargetHttpProxy("http-proxy", url_map=redirect_url_map.id)
+    http_forwarding_rule = compute.GlobalForwardingRule("http-forwarding-rule", ip_address=ip.address, ip_protocol="TCP", port_range="80", target=http_proxy.id, load_balancing_scheme="EXTERNAL_MANAGED")
+
+
 pulumi.export("bucket_name", bucket.url)
 pulumi.export("url", pulumi.Output.concat("http://storage.googleapis.com/", bucket.id, "/", "index.html"))
+
 
